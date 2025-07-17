6a4af3269ca5af297292a2383456823a3ff24d82
Sunil Jain <sjain@apptech.insure>
2025-07-17 12:26:37 -0400

    added register_resource_creator to main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 import asyncio
 
 from viam.module.module import Module
+from viam.resource.registry import Registry, ResourceCreatorRegistration
+from viam.services.vision import Vision
+
+# Import the vision service class
 from yolov8 import yolov8
+
+# Register the model with the Viam registry
+Registry.register_resource_creator(
+    Vision.SUBTYPE,
+    yolov8.MODEL,
+    ResourceCreatorRegistration(yolov8.new, yolov8.validate_config),
+)
 
 if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
